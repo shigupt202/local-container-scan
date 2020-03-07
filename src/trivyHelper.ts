06588d8647fb167475b6cb30f10af8b923933b22
Shivam Gupta <shigupt@microsoft.com>
2020-03-07 15:01:58 +0530

    Updating trivy version format

--- src/trivyHelper.ts
+++ src/trivyHelper.ts
@@ -5,7 +5,7 @@ import * as fs from 'fs';
 import * as toolCache from '@actions/tool-cache';
 import * as core from '@actions/core';
 
-const stableTrivyVersion = "v.0.5.0";
+const stableTrivyVersion = "0.5.0";
 const trivyLatestReleaseUrl = "https://api.github.com/repos/aquasecurity/trivy/releases/latest";
 const trivyToolName = "trivy";
 
@@ -41,7 +41,7 @@ async function getLatestTrivyVersion(): Promise<string> {
             return stableTrivyVersion;
         }
 
-        return response.tag_name;
+        return response.tag_name.substring(1);
     }, (error) => {
         core.debug(error);
         core.warning(util.format("Failed to read latest trivy verison from %s. Using default stable version %s", trivyLatestReleaseUrl, stableTrivyVersion));
@@ -53,10 +53,10 @@ function getTrivyDownloadUrl(trivyVersion: string): string {
     const curOS = os.type();
     switch (curOS) {
         case "Linux":
-            return util.format("https://github.com/aquasecurity/trivy/releases/download/%s/trivy_%s_Linux-32bit.tar.gz", trivyVersion, trivyVersion);
+            return util.format("https://github.com/aquasecurity/trivy/releases/download/v%s/trivy_%s_Linux-32bit.tar.gz", trivyVersion, trivyVersion);
 
         case "Darwin":
-            return util.format("https://github.com/aquasecurity/trivy/releases/download/%s/trivy_%s_macOS-32bit.tar.gz", trivyVersion, trivyVersion);
+            return util.format("https://github.com/aquasecurity/trivy/releases/download/v%s/trivy_%s_macOS-32bit.tar.gz", trivyVersion, trivyVersion);
 
         default:
             throw new Error(util.format("Container scanning is not supported for %s currently", curOS));

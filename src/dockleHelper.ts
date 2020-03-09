a7b8b90a50654c1c5ef093160f2d511165de1322
Shivam Gupta <shigupt@microsoft.com>
2020-03-09 23:43:51 +0530

    CHanging download dir again

--- src/dockleHelper.ts
+++ src/dockleHelper.ts
@@ -15,7 +15,7 @@ export async function getDockle(): Promise<string> {
     if (!cachedToolPath) {
         let dockleDownloadPath;
         const dockleDownloadUrl = getDockleDownloadUrl(latestDockleVersion);
-        const dockleDownloadDir = `${process.env['GITHUB_WORKSPACE']}/_temp/tools/dockle`;
+        const dockleDownloadDir = `${process.env['GITHUB_WORKSPACE']}/_temp/tools`;
         console.log(util.format("Could not find dockle in cache, downloading from %s", dockleDownloadUrl));
 
         try {

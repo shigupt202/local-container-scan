697838331996b912f9fd6ac4ec2a88f2811b297c
Shivam Gupta <shigupt@microsoft.com>
2020-03-09 21:53:35 +0530

    Adding dockle support

---
+++ src/dockleHelper.ts
@@ -0,0 +1,94 @@
+import * as os from 'os';
+import * as path from 'path';
+import * as util from 'util';
+import * as fs from 'fs';
+import * as toolCache from '@actions/tool-cache';
+import * as core from '@actions/core';
+
+const stableDockleVersion = "0.2.4";
+const dockleLatestReleaseUrl = "https://api.github.com/repos/goodwithtech/dockle/releases/latest";
+const dockleToolName = "dockle";
+
+export async function getDockle(): Promise<string> {
+    const latestDockleVersion = await getLatestDockleVersion();
+    let cachedToolPath = toolCache.find(dockleToolName, latestDockleVersion);
+    if (!cachedToolPath) {
+        let dockleDownloadPath;
+        const dockleDownloadUrl = getDockleDownloadUrl(latestDockleVersion);
+        const dockleDownloadDir = `${process.env['GITHUB_WORKSPACE']}/_temp/tools`;
+        console.log(util.format("Could not find dockle in cache, downloading from %s", dockleDownloadUrl));
+
+        try {
+            dockleDownloadPath = await toolCache.downloadTool(dockleDownloadUrl, dockleDownloadDir);
+        } catch (error) {
+            throw new Error(util.format("Failed to download dockle from %s", dockleDownloadUrl));
+        }
+
+        fs.chmodSync(dockleDownloadPath, "777");
+        const untarredDocklePath = await toolCache.extractTar(dockleDownloadPath);
+        cachedToolPath = await toolCache.cacheDir(untarredDocklePath, dockleToolName, latestDockleVersion);
+    }
+
+    const dockleToolPath = findDockle(cachedToolPath);
+    fs.chmodSync(dockleToolPath, "777");
+
+    return dockleToolPath;
+}
+
+async function getLatestDockleVersion(): Promise<string> {
+    return toolCache.downloadTool(dockleLatestReleaseUrl).then((downloadPath) => {
+        const response = JSON.parse(fs.readFileSync(downloadPath, 'utf8').toString().trim());
+        if (!response.tag_name) {
+            return stableDockleVersion;
+        }
+
+        return response.tag_name.substring(1);
+    }, (error) => {
+        core.debug(error);
+        core.warning(util.format("Failed to read latest dockle verison from %s. Using default stable version %s", dockleLatestReleaseUrl, stableDockleVersion));
+        return stableDockleVersion;
+    });
+}
+
+function getDockleDownloadUrl(dockleVersion: string): string {
+    const curOS = os.type();
+    switch (curOS) {
+        case "Linux":
+            return util.format("https://github.com/goodwithtech/dockle/releases/download/v%s/dockle_%s_Linux-32bit.tar.gz", dockleVersion, dockleVersion);
+
+        case "Darwin":
+            return util.format("https://github.com/goodwithtech/dockle/releases/download/v%s/dockle_%s_Linux-32bit.tar.gz", dockleVersion, dockleVersion);
+
+        default:
+            throw new Error(util.format("Container scanning is not supported for %s currently", curOS));
+    }
+}
+
+function findDockle(rootFolder: string): string {
+    fs.chmodSync(rootFolder, '777');
+    var filelist: string[] = [];
+    walkSync(rootFolder, filelist, dockleToolName);
+    if (!filelist) {
+        throw new Error(util.format("Dockle executable not found in path %s", rootFolder));
+    }
+    else {
+        return filelist[0];
+    }
+}
+
+var walkSync = function (dir: string, filelist: string[], fileToFind: any) {
+    var files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function (file) {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            filelist = walkSync(path.join(dir, file), filelist, fileToFind);
+        }
+        else {
+            core.debug(file);
+            if (file == fileToFind) {
+                filelist.push(path.join(dir, file));
+            }
+        }
+    });
+    return filelist;
+};

6e340ceaecd1df70c28267a50218a52faefe9c6b
Shivam Gupta <shigupt@microsoft.com>
2020-03-20 13:31:36 +0530

    Removing github token input

--- src/inputHelper.ts
+++ src/inputHelper.ts
@@ -1,7 +1,6 @@
 import * as core from '@actions/core';
 
 export const imageName = core.getInput("image-name");
-export const githubToken = core.getInput("github-token");
 export const username = core.getInput("username");
 export const password = core.getInput("password");
 export const severityThreshold = core.getInput("severity-threshold");
